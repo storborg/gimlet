7a199ae85299880c92a765d55d7758dd312d60fc
Wyatt Baldwin <wyatt@cartlogic.com>
2013-02-12 20:03:25 -0800

    Discontinue use of SQLA sessions in SQL back end

--- gimlet/backends/sql.py
+++ gimlet/backends/sql.py
@@ -1,5 +1,4 @@
 from sqlalchemy import MetaData, Table, Column, types, create_engine, select
-from sqlalchemy.orm import sessionmaker, scoped_session
 
 from .base import BaseBackend
 
@@ -10,8 +9,6 @@ class SQLBackend(BaseBackend):
         engine = create_engine(url)
         meta = MetaData()
         meta.bind = engine
-        sm = sessionmaker(bind=engine)
-        self.sess = scoped_session(sm)
 
         self.table = Table(table_name, meta,
                            Column('id', types.Integer, primary_key=True),
@@ -22,33 +19,25 @@ class SQLBackend(BaseBackend):
 
     def __setitem__(self, key, value):
         raw = self.serialize(value)
-        try:
-            # Check if this key exists with a SELECT FOR UPDATE, to protect
-            # against a race with other concurrent writers of this key.
-            q = self.table.select('1', for_update=True).\
-                where(self.table.c.key == key)
-            r = self.sess.execute(q).fetchone()
-            if r:
-                # If it exists, use an UPDATE.
-                q = self.table.update().\
-                    values(data=raw).\
-                    where(self.table.c.key == key)
-            else:
-                # Otherwise INSERT.
-                q = self.table.insert().values(key=key, data=raw)
-            self.sess.execute(q)
-            self.sess.commit()
-        finally:
-            self.sess.remove()
+
+        # Check if this key exists with a SELECT FOR UPDATE, to protect
+        # against a race with other concurrent writers of this key.
+        r = self.table.select('1', for_update=True).\
+            where(self.table.c.key == key).execute().fetchone()
+
+        if r:
+            # If it exists, use an UPDATE.
+            self.table.update().values(data=raw).\
+                where(self.table.c.key == key).execute()
+        else:
+            # Otherwise INSERT.
+            self.table.insert().values(key=key, data=raw).execute()
 
     def __getitem__(self, key):
-        try:
-            q = select([self.table.c.data], self.table.c.key == key)
-            r = self.sess.execute(q).fetchone()
-            if r:
-                raw = r[0]
-                return self.deserialize(raw)
-            else:
-                raise KeyError('key %r not found' % key)
-        finally:
-            self.sess.remove()
+        r = select([self.table.c.data], self.table.c.key == key).\
+            execute().fetchone()
+        if r:
+            raw = r[0]
+            return self.deserialize(raw)
+        else:
+            raise KeyError('key %r not found' % key)
