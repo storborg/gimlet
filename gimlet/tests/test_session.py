6cc39c6067c06742c886b32560260f2a297162f0
Wyatt Baldwin <wyatt@cartlogic.com>
2013-02-22 15:29:13 -0800

    Add functional tests for session
    In particular, to test session invalidation.

--- gimlet/tests/test_session.py
+++ gimlet/tests/test_session.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
-from webob import Request
+from webob import Request, Response
+
+from webtest import TestApp
 
 from gimlet.factories import session_factory_factory
 
@@ -51,3 +53,56 @@ class TestSession(TestCase):
         token = sess.get_csrf_token()
         self.assertIn('_csrft_', sess)
         self.assertEqual(token, sess.get_csrf_token())
+
+
+class App(object):
+
+    def __init__(self):
+        self.session_factory = session_factory_factory('secret')
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        request.session = self.session_factory(request)
+        view_name = request.path_info_pop()
+        view = getattr(self, view_name)
+        response = view(request)
+        request.session.response_callback(request, response)
+        return response(environ, start_response)
+
+    def get(self, request):
+        return Response('get')
+
+    def set(self, request):
+        request.session['a'] = 'a'
+        return Response('set')
+
+    def invalidate(self, request):
+        request.session.invalidate()
+        return Response('invalidate')
+
+
+class TestSession_Functional(TestCase):
+
+    def setUp(self):
+        self.app = TestApp(App())
+
+    def test_invalidate(self):
+        # First request has no cookies; this sets them
+        res = self.app.get('/set')
+        self.assertEqual(res.request.cookies, {})
+        self.assertIn('Set-Cookie', res.headers)
+        # Next request should contain cookies
+        res = self.app.get('/get')
+        self.assert_(res.request.cookies)
+        self.assertIn('gimlet', res.request.cookies)
+        old_cookie_value = res.request.cookies['gimlet']
+        self.assert_(old_cookie_value)
+        # Invalidation should empty the session and set a new cookie
+        res = self.app.get('/invalidate')
+        self.assertIn('Set-Cookie', res.headers)
+        self.assertEqual(res.request.session, {})
+        res = self.app.get('/get')
+        self.assertIn('gimlet', res.request.cookies)
+        new_cookie_value = res.request.cookies['gimlet']
+        self.assert_(new_cookie_value)
+        self.assertNotEqual(new_cookie_value, old_cookie_value)
